b909d38b630190167ac313f204a9b70176e77f8f
its-me-mohan <156092217+its-me-mohan@users.noreply.github.com>
2024-01-09 18:53:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Login from './Component/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <h1>HeLLO WORLD</h1>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Login.js
@@ -0,0 +1,9 @@
+import './Login.css';
+
+export default function Login(){
+    return(
+        <div>
+            <h1>login</h1>
+        </div>
+    )
+}
